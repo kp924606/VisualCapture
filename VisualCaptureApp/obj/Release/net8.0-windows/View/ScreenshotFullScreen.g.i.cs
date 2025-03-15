0a54649f2465063e7bd6ccd4d73c8cb2e3d4ff8f
TCT <kp924606@gmail.com>
2025-03-15 12:59:13 +0800

    Add and Update

--- VisualCaptureApp/obj/Release/net8.0-windows/View/ScreenshotFullScreen.g.i.cs
+++ VisualCaptureApp/obj/Release/net8.0-windows/View/ScreenshotFullScreen.g.i.cs
@@ -54,7 +54,7 @@ namespace VisualCaptureApp.View {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/VisualCaptureApp;V1.0.0.0;component/view/screenshotfullscreen.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/VisualCaptureApp;component/view/screenshotfullscreen.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\View\ScreenshotFullScreen.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
